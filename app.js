e14e19d48c6454e0108c0242ded98b1da681a1b2
JudithCortes <baruki110214@gmail.com>
2018-08-31 20:54:52 +0200

    avances 31-agosto

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+
+//servidor local
+//los modulos de node se pueden importar al script del servidor usando require
+//libreria que vamos a importar es http que se encarga de servir apps web
 var http = require("http");
 
-var manejador = function(solicitud,respuesta){
+var  = function(sol,res){
 console.log("recibimos una nueva peticion");
 respuesta.end("hola mundo");
 };
 //decirle en que puerto se ejecuta
 //retorna un objeto 
 var servidor = http.createServer(manejador);
-servidor.listen(8080);
+servidor.listen(3000);
 
--- app_html.js
+++ app_html.js
@@ -1,11 +1,14 @@
+//montar un servidor donde se sirve un archivo
+//se hace require de http
 var http = require("http"),
+//se usa modulo de node fs(fileSystem) que se comunica con el sistema de archivos de la computadora
 fs = require("fs");
 //guardamos en una variable lo que retorna
 fs.readFile("./index.html", function(err,html){
     http.createServer(function(req,res){
         res.write(html);
         res.end();
-        }).listen(8080);
+        }).listen(3000);
 });
 
 
